0a049b82b6f7b01ff7c555918ed3d02611cc294f
suha <gusuhawoo@gmail.com>
2023-03-13 21:44:52 +0900

    Add : Create & Read(CLUD)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/index' => 'home#index'
+  get '/write' => 'home#write'
+  post '/create' => 'home#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
