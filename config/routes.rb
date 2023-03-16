032df97d7e96795b57d75b831795ceb6721cefbd
suha <gusuhawoo@gmail.com>
2023-03-16 13:42:09 +0900

    Add : update(modify)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '/index' => 'home#index'
   get '/write' => 'home#write'
   post '/create' => 'home#create'
+
+  get '/modify/:post_id' => 'home#modify'
+  post '/update/:post_id' => 'home#update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
